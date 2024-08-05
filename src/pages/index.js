c53bffc77a34703ecc72ad13084fdd9b6312d2b3
Gabo <gabo5612@gmail.com>
2024-08-05 11:34:02 -0400

    Test

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,13 +1,12 @@
 import * as React from "react"
-import Navbar from "../components/navbar"
+import Navbar from "../components/Navbar"
 import FirstView from "../components/FirstView"
-import '../styles/global.css'
-import About from "../components/about"
+import About from "../components/About"
 import MySkill from "../components/MySkills"
 import Porfolio from "../components/Portfolio"
 import Experience from "../components/Experience"
-import Footer from "../components/footer"
-
+import Footer from "../components/Footer"
+import '../styles/global.css'
 
 const IndexPage = () => {
   return (
